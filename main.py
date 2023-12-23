fe3cff24474e5a039ae850db89908564e9e8a3c7
zxymiku <zxymiku@outlook.com>
2023-12-23 22:36:25 +0800

    fd

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("none")
+import tkinter as tk
+
+def button_click():
+    pass
+
+root = tk.Tk()
+root.title("My Window")
+
+button = tk.Button(root, text="Click me!", command=button_click)
+button.pack()
+
+root.mainloop()
