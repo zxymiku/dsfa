4b725e8c27bc4c0d726cb9191de531dcca1b6140
zxymiku <zxymiku@outlook.com>
2023-12-23 22:37:11 +0800

    Revert "fd"
    This reverts commit fe3cff24474e5a039ae850db89908564e9e8a3c7.

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
 print("none")
-import tkinter as tk
-
-def button_click():
-    pass
-
-root = tk.Tk()
-root.title("My Window")
-
-button = tk.Button(root, text="Click me!", command=button_click)
-button.pack()
-
-root.mainloop()
